57dadd7e20beb0b10d04ebe3070ea91bfd610d1b
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-11 19:24:01 +0900

    Fix: Travel의 grade 타입 변경

--- SeSAC7Week3_T1/Model/Travel.swift
+++ SeSAC7Week3_T1/Model/Travel.swift
@@ -7,7 +7,7 @@ struct Travel {
     let title: String
     let description: String
     let travel_image: String
-    let grade: Int
+    let grade: Double
     let save: Int
     let like: Bool
     let ad: Bool
