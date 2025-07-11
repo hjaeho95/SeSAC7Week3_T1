e21cce0868dbfeb08ca70e88c769048dab03f309
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-11 19:19:52 +0900

    Feat: TravelInfo 화면 초기 구성

---
+++ SeSAC7Week3_T1/ViewController/TravelInfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelInfoTableViewCell.swift
+//  SeSAC7Week3_T1
+//
+//  Created by ez on 7/11/25.
+//
+
+import UIKit
+
+class TravelInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
