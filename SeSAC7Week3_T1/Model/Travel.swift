503975a82804553069219883be8557e4cd850ca4
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-11 19:22:46 +0900

    Refactor: 구조체 파일명 변경, Travel 데이터 입력

---
+++ SeSAC7Week3_T1/Model/Travel.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+
+
+
+struct Travel {
+    let title: String
+    let description: String
+    let travel_image: String
+    let grade: Int
+    let save: Int
+    let like: Bool
+    let ad: Bool
+}
