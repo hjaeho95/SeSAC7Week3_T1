99aa4768f829727dca81ceb65b272258f5bb45f0
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-11 15:40:33 +0900

    Feat: Logic 과제를 위한 스토리보드, 뷰 컨트롤러 생성

---
+++ SeSAC7Week3_T1/ViewController/LogicViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LogicViewController.swift
+//  SeSAC7Week3_T1
+//
+//  Created by ez on 7/11/25.
+//
+
+import UIKit
+
+class LogicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
