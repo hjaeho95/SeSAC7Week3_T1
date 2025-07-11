8c86dccaa85260c2f7e1b61068c3d93269e3bb36
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-11 19:26:00 +0900

    Fix: Travel 구조체의 멤버 타입에 옵셔널 추가

--- SeSAC7Week3_T1/Model/Travel.swift
+++ SeSAC7Week3_T1/Model/Travel.swift
@@ -4,11 +4,11 @@ import Foundation
 
 
 struct Travel {
-    let title: String
-    let description: String
-    let travel_image: String
-    let grade: Double
-    let save: Int
-    let like: Bool
+    let title: String?
+    let description: String?
+    let travel_image: String?
+    let grade: Double?
+    let save: Int?
+    let like: Bool?
     let ad: Bool
 }
